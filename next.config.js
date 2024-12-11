5106a63e1e7ed535db68d7042f48a5bdff875c91
Michael Allen Elguira <AllenElguira16@gmail.com>
2024-12-11 22:23:31 +0800

    fix: fixed incorrect destination

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         source: '/user',
-        destination: '/user/dashboard',
+        destination: '/user/appointment',
         permanent: true,
       },
       {
